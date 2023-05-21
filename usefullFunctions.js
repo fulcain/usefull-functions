e94b3aaf8fc7c9e1c54f357bd00bca41fa94989d
Majid Kargar <majidkargar01@gmail.com>
2023-05-22 00:05:24 +0330

    refactor(): delete 2 functions

--- usefullFunctions.js
+++ usefullFunctions.js
@@ -1,32 +1,8 @@
 /**
- * adds comma to numbers after 3 every 3 digit
- * @param {*} number - a number given in either string or number
- * @example 
- * // returns 100,000,000
- * addCommaToNumber(100000000)
- * @returns {Number} - a comma separated number
- */
-function addCommaToNumber(number) {
-    return Number(String(number).replace(/[^0-9]/g, '')).toLocaleString()
-}
-
-/**
- * add zero to one digit numbers
- * @param {string|number} value - one digit number 
- * @example
- * // returns 02
- * addZero(2)
- * @returns {string|number} - two digit number
- */
-function addZero(value) {
-    return value < 10 ? ('0' + value).slice(-2) : value;
-}
-
-/**
  * Convert any number to english digits with requested format
  * @param {string|number} str - string or number of numbers
- * @param {string} type - type of input in output  
- * @example 
+ * @param {string} type - type of input in output
+ * @example
  * // returns "123"
  * convertAnyNumberToEnglish("123","string")
  * @returns {string|number} - converted any number to english number
@@ -45,6 +21,7 @@ function convertAnyNumberToEnglish(input, type) {
     for (let i = 0; i < 10; i++) {
         input = input.replaceAll(persianNumbers[i], i).replaceAll(arabicNumbers[i], i);
     }
+
     // if type is number parses the input into number
     return type === "number" ? Number(input) : input
 }
