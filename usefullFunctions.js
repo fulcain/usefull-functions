d25a29d3da41c90a2c3cac7c881311645d5b9c71
Majid Kargar <majidkargar01@gmail.com>
2023-05-22 17:07:24 +0430

    feat(): add xhrApiCall function

--- usefullFunctions.js
+++ usefullFunctions.js
@@ -2,26 +2,71 @@
  * Convert any number to english digits with requested format
  * @param {string|number} str - string or number of numbers
  * @param {string} type - type of input in output
- * @example
- * // returns "123"
- * convertAnyNumberToEnglish("123","string")
  * @returns {string|number} - converted any number to english number
  */
 function convertAnyNumberToEnglish(input, type) {
-    // changes the input to string
-    input = String(input)
-    type = type.toLowerCase()
+	// changes the input to string
+	input = String(input);
+	type = type.toLowerCase();
 
-    // array of numbers
-    const
-        persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'],
-        arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+	// array of numbers
+	const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"],
+		arabicNumbers = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
 
-    // for loop to replace number
-    for (let i = 0; i < 10; i++) {
-        input = input.replaceAll(persianNumbers[i], i).replaceAll(arabicNumbers[i], i);
-    }
+	// for loop to replace number
+	for (let i = 0; i < 10; i++) {
+		input = input
+			.replaceAll(persianNumbers[i], i)
+			.replaceAll(arabicNumbers[i], i);
+	}
 
-    // if type is number parses the input into number
-    return type === "number" ? Number(input) : input
+	// if type is number parses the input into number
+	return type === "number" ? Number(input) : input;
 }
+/**
+ * a function to receive data from an API
+ * @param {string} method - the HTTP method (e.g. `GET`, `POST`, `PUT`, etc.)
+ * @param {string} url - the API endpoint URL
+ * @param {object|array} data - optional data to send with the request (e.g. an object or array).
+ */
+function xhrApiCall(method,url,data) {
+	return new Promise((resolve, reject) => {
+
+		// create xhr
+		const xhr = new XMLHttpRequest();
+
+		// xhr open method
+		xhr.open(method, url);
+
+		// xhr on load function
+		xhr.onload = function () {
+
+            // if the status is in 200 - 299 range this code will be executed
+			if (this.status >= 200 && this.status < 300) {
+				resolve(JSON.parse(this.response));
+			} else {
+				reject({
+					status: this.status,
+					statusText: this.statusText
+				});
+			}
+		};
+
+        // if there is an error while getting data
+		xhr.onerror = function () {
+			reject({
+				status: this.status,
+				statusText: this.statusText
+			});
+		};
+
+        // send the xhr request with an optional data parameter which is either an object or array
+		xhr.send(JSON.stringify(data));
+	});
+}
+
+xhrApiCall("GET","https://api.namefake.com/","{name:jasem}").then(
+    (data) =>{
+        console.log(data);
+    }
+)
